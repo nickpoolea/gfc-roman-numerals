e8169df9c258529968ff5d9c3bdd7b8fe22d68ac
Nick Poole <NickPooleA@gmail.com>
2017-08-22 18:14:28 -0700

    Finalized the conversion loop.

--- src/main/java/com/liberymutual/goforcode/romanNumerals/services/HinduToRomanNumeralConverter.java
+++ src/main/java/com/liberymutual/goforcode/romanNumerals/services/HinduToRomanNumeralConverter.java
@@ -4,42 +4,48 @@ public class HinduToRomanNumeralConverter {
 
 	public String convert(int input) {
 		
-//		String result = "";
-//		
-//		if (input / 10 >= 1) {
-//			for (int i = 1; i <= Math.floor(input/10); i++) {
-//				result += "X";
-//			}
-//		}
-//		System.out.print(result);
-//		return result;
-		
 		String[] characters = new String[] {"I", "V", "X", "L", "C", "D", "M"};
 		String result = "";
+		int numberToAnalyze;
+		int arrayIndex = 0;
 		
-		if (input < 4) {
-			for (int i = 0; i < input; i++) {
-				result += characters[0];
+		while (input > 0) {
+			
+			//Loop through each individual digit
+			numberToAnalyze = input % 10;
+		
+			if (numberToAnalyze == 0) {
+				//do nothing
 			}
-		}
-		
-		else if (input == 4) {
-			result += characters[0] + characters[1];
-		}
-		
-		else if (input == 5) {
-			result += characters[1];
-		}
-		
-		else if (input < 9) {
-			result += characters[1];
-			for (int i = 0; i < input - 5; i++) {
-				result += characters[0];
+			
+			else if (numberToAnalyze < 4) {
+				for (int j = 0; j < numberToAnalyze; j++) {
+					result = characters[arrayIndex] + result;
+				}
 			}
-		}
-			else if (input == 9) {
-				result += characters[2] + characters[1];
+			
+			else if (numberToAnalyze == 4) {
+				result = characters[arrayIndex] + characters[arrayIndex + 1] + result;
 			}
+			
+			else if (numberToAnalyze == 5) {
+				result = characters[arrayIndex + 1] + result;
+			}
+			
+			else if (numberToAnalyze < 9) {
+				for (int j = 0; j < numberToAnalyze - 5; j++) {
+					result = characters[arrayIndex] + result;
+				}
+				result = characters[arrayIndex + 1] + result;
+			}
+			
+			else if (numberToAnalyze == 9) {
+				result = characters[arrayIndex] + characters[arrayIndex + 2] + result;
+			}
+			
+			arrayIndex += 2;
+			input /= 10;
+						
 		}
 		
 		return result;
